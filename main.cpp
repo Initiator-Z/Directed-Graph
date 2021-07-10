73452c2f07bb19a3899258c2c85c1f8cf93e6b90
Initiator-Z <60876834+Initiator-Z@users.noreply.github.com>
2021-07-10 13:50:33 +1000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,7 +12,6 @@
 int main() {
     directed_graph<int> g;
 
-
     g.add_vertex(vertex(1,800));
     g.add_vertex(vertex(2,300));
     g.add_vertex(vertex(3,400));
@@ -33,55 +32,22 @@ int main() {
     g.add_edge(1,7,500);
     g.add_edge(8,7,500);
 
+    //Returns the vertices of the graph in the order they are visited in by a depth-first traversal 
+    //starting at the given vertex.
+    g.depth_first(1); 
+    
+    //Returns a spanning tree of the graph starting at the given vertex using the out-edges. 
+    //This means every vertex in the tree is reachable from the root.
+    g.out_tree(2); 
 
-    /**
-    g.add_vertex(vertex(1,800));
-    g.add_vertex(vertex(2,3000));
-    g.add_vertex(vertex(3,400));
-    g.add_vertex(vertex(4,710));
-    g.add_vertex(vertex(5,221));
-    g.add_vertex(vertex(6,800));
-    g.add_vertex(vertex(7,3000));
-    g.add_vertex(vertex(8,400));
-    g.add_vertex(vertex(9,710));
+    //Computes the shortest distance from 1 to 8 in the graph g
+    shortest_path(g, 1, 8)
+    
+    //Test for strongly comment componnets in the given graph
+    strongly_connected_components(g);
 
-    g.add_edge(1,2,8);
-    g.add_edge(1,3,3);
-    g.add_edge(1,6,13);
-    g.add_edge(2,3,2);
-    g.add_edge(2,4,1);
-    g.add_edge(3,2,3);
-    g.add_edge(3,4,9);
-    g.add_edge(3,5,2);
-
-
-    g.add_edge(4,5,4);
-    g.add_edge(4,8,2);
-    g.add_edge(4,7,6);
-    g.add_edge(5,4,6);
-    g.add_edge(5,1,5);
-    g.add_edge(5,6,5);
-    g.add_edge(5,9,4);
-
-    g.add_edge(6,9,7);
-    g.add_edge(6,7,1);
-    g.add_edge(7,5,3);
-    g.add_edge(7,8,4);
-    g.add_edge(8,9,1);
-    g.add_edge(9,7,5);
-    **/
-
-
-    int a = 1;
-    int b = 5;
-    test_topo(g);
-
-
-    //strongly_connected_components(g);
-
-    //topological_sort(g);
-    //cout << low_cost_delivery(g, a);
-
-    //cout << g.get_edge(3,4) << endl;
+    //Computes a topological ordering of the vertices in the given graph. 
+    //A Directed acyclic graph(DAG) is required.
+    topological_sort(g);
 
 }
